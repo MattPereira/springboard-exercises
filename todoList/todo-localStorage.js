09a9662828f10a1da1cfb87407fc424657384138
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-01-04 10:01:21 -0800

    partial localStorage success

--- todoList/todo-localStorage.js
+++ todoList/todo-localStorage.js
@@ -6,17 +6,34 @@ const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 for (let i = 0; i < savedTodos.length; i++) {
   let newTodo = document.createElement("li");
   newTodo.innerText = savedTodos[i].task;
+  todoList.append(newTodo);
+
+  let completeBtn = document.createElement("button");
+  completeBtn.innerHTML = "&check; complete";
+  completeBtn.classList.add("btn", "btn-success");
+
+  let removeBtn = document.createElement("button");
+  removeBtn.innerHTML = "&#10005; delete";
+  removeBtn.classList.add("btn", "btn-danger");
+
+  newTodo.append(completeBtn, removeBtn);
+
+  //mark todo as completed
+  completeBtn.addEventListener("click", function () {
+    completeBtn.parentElement.classList.toggle("text-decoration-line-through");
+  });
+
+  //remove a todo
+  removeBtn.addEventListener("click", function () {
+    removeBtn.parentElement.remove();
+  });
 }
 
 todoForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  let newTodo = document.createElement("li");
   let taskValue = document.getElementById("task").value;
-  //   newTodo.innerText = taskValue;
-  //   todoForm.reset();
-  //   todoList.appendChild(newTodo);
 
   // save to localStorage
-  savedTodos.push({ task: newTodo.innerText, isCompleted: false });
+  savedTodos.push({ task: taskValue });
   localStorage.setItem("todos", JSON.stringify(savedTodos));
 });
