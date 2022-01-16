4c5572a3006701a76abe386752776c3c664ae0c0
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-01-15 17:50:05 -0800

    add let and const exercise

---
+++ let-const-exercise/app.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+
+//What is the difference between var and let?
+let answer1 = "var is function scoped, but let is block scoped";
+
+//What is the difference between var and const?
+let answer2 =
+  "var is function scoped, but const is block scoped. Also var can be declared without initialization, while const cannot";
+
+//What is the difference between let and const?
+let answer3 =
+  "Although both are block scope, let can be redeclared, but const cannot";
+
+//What is hoisting??
+let answer4 =
+  "hoisting is th eprocess whereby the interpreter appears to move the declaration of functions, variables, or classes to the top of their scope, prior to execution of the code. Hoisting allows functions to be safely used in code before they are declared";
