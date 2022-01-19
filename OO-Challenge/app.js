5a1c0771252307d20936a7158ed1a5403abbdf87
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-01-18 16:44:37 -0800

    add Car OO Project

---
+++ OO-Challenge/app.js
@@ -0,0 +1,53 @@
+//PART ONE
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    return "Beep";
+  }
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+  }
+}
+
+//PART TWO
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+// PART THREE
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    return "VROOM!!!";
+  }
+}
+
+class Garage {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.vehicles = [];
+  }
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle Added!";
+  }
+}
