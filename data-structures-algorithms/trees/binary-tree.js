728d734131a031cc9d0db21366bcd0897e858085
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-07-10 11:13:13 -0700

    trying to understand further study solutions

--- data-structures-algorithms/trees/binary-tree.js
+++ data-structures-algorithms/trees/binary-tree.js
@@ -57,7 +57,7 @@ class BinaryTree {
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
-    let sum = 0;
+    let result = 0;
 
     function maxSumHelper(node) {
       if (node === null) return 0;
@@ -105,23 +105,125 @@ class BinaryTree {
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {}
+  areCousins(node1, node2) {
+    //if either node is the root they can't be cousins
+    if (node1 === this.root || node2 === this.root) return false;
+
+    function findLevelAndParent(
+      nodeToFind,
+      currentNode,
+      level = 0,
+      data = { level: 0, parent: null }
+    ) {
+      if (data.parent) return data;
+
+      if (currentNode.left === nodeToFind || currentNode.right === nodeToFind) {
+        data.level = level + 1;
+        data.parent = currentNode;
+      }
+      if (currentNode.left) {
+        findLevelAndParent(nodeToFind, currentNode.left, level + 1, data);
+      }
+      if (currentNode.right) {
+        findLevelAndParent(nodeToFind, currentNode.right, level + 1, data);
+      }
+      return data;
+    }
+
+    let node1Info = findLevelAndParent(node1, this.root);
+    let node2Info = findLevelAndParent(node2, this.root);
+
+    let sameLevel =
+      node1Info && node2Info && node1Info.level === node2Info.level;
+
+    let differentParents =
+      node1Info && node2Info && node1Info.parent !== node2Info.parent;
+
+    return sameLevel && differentParents;
+  }
 
   /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
+   * serialize(tree): serialize the BinaryTree object tree into a string.
+   *
+   * Output looks like this
+   *
+   *  1,2,null,null,3,4,null,null,5,null,null
+   *
+   *  where null represents null children with a pre-order traversal
+   *
+   * */
 
-  static serialize() {}
+  static serialize(tree) {
+    const values = [];
+
+    function traverse(node) {
+      if (node) {
+        values.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+      } else {
+        values.push("null");
+      }
+    }
+
+    traverse(tree.root);
+    return values.join(",");
+  }
 
   /** Further study!
    * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  static deserialize() {}
+  static deserialize(stringTree) {
+    if (!stringTree) return null;
+
+    const values = stringTree.split(",");
+
+    function buildTree() {
+      // building a tree starting from the beginning of the array
+      if (values.length) {
+        const currentVal = values.shift();
+
+        if (currentVal === "null") return null;
+
+        //remember to convert values back into numbers
+        let currentNode = new BinaryTreeNode(+currentVal);
+        currentNode.left = buildTree();
+        currentNode.right = buildTree();
+
+        return currentNode;
+      }
+    }
+
+    const root = buildTree();
+    return new BinaryTree(root);
+  }
 
   /** Further study!
    * lowestCommonAncestor(node1, node2): find the lowest common ancestor
    * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {}
+  lowestCommonAncestor(node1, node2, currentNode = this.root) {
+    // base case 1: empty tree
+    if (currentNode === null) return null;
+
+    // base case 2: root is one of the target nodes
+    if (currentNode === node1 || currentNode === node2) return currentNode;
+
+    // recursively search the left sub-tree
+    const left = this.lowestCommonAncestor(node1, node2, currentNode.left);
+
+    // recursively search the right sub-tree
+    const right = this.lowestCommonAncestor(node1, node2, currentNode.right);
+
+    // if neither left nor right is null, currentNode is the ancestor
+    if (left !== null && right !== null) return currentNode;
+
+    // if one node is not null, return it
+    if (left !== null || right !== null) return left || right;
+
+    // left and right are both null, return null
+    if (left === null && right === null) return null;
+  }
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
