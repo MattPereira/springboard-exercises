a1f56324883080cbae099a49186af62549501649
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-07-20 13:23:54 -0700

    finish tricky parts exercise

--- outcomes-js-tricky-parts/create-account.js
+++ outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,30 @@
 function createAccount(pin, amount) {
+  let balance = amount || 0;
+  let pinNum = pin;
 
+  return {
+    checkBalance: function (pin) {
+      if (pin !== pinNum) return `Invalid PIN.`;
+      return `$${balance}`;
+    },
+    deposit: function (num, depositAmount) {
+      if (num !== pinNum) return `Invalid PIN.`;
+      balance += depositAmount;
+      return `Succesfully deposited $${depositAmount}. Current balance: $${balance}.`;
+    },
+    withdraw: function (pin, withdrawAmount) {
+      if (pin !== pinNum) return `Invalid PIN.`;
+      if (withdrawAmount > balance)
+        return `Withdrawal amount exceeds account balance. Transaction cancelled.`;
+      balance -= withdrawAmount;
+      return `Succesfully withdrew $${withdrawAmount}. Current balance: $${balance}.`;
+    },
+    changePin: function (pin, newPin) {
+      if (pin !== pinNum) reeturn`Invalid PIN.`;
+      pinNum = newPin;
+      return `PIN successfully changed!`;
+    },
+  };
 }
 
 module.exports = { createAccount };
--- outcomes-js-tricky-parts/guessing-game.js
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,23 @@
 function guessingGame() {
+  let target = Math.floor(Math.random() * 100);
+  let guesses = 0;
+  let alreadyWon = false;
 
+  return function guess(num) {
+    if (alreadyWon) return `The game is over, you already won!`;
+
+    if (num > target) {
+      guesses++;
+      return `${num} is too high!`;
+    } else if (num < target) {
+      guesses++;
+      return `${num} is too low!`;
+    } else if (num === target) {
+      guesses++;
+      alreadyWon = true;
+      return `You win! You found ${target} in 3 guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
